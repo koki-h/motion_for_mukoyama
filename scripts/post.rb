f48b7120a2dc5b3bc808ae604eaf99cbdccb8ef2
koki_h <gohannnotomo@gmail.com>
2017-06-15 21:51:52 +0900

    mukoyamaサーバへ送信するタイムスタンプを送信時の時刻でなく、画像ファイル名から取得するようにした。 サーバのファイル名と画像に写し込まれた時刻がズレる現象を解決

--- scripts/post.rb
+++ scripts/post.rb
@@ -1,10 +1,21 @@
 #!/usr/bin/env ruby
 def send_picture
-  ts = Time.now.strftime("%Y-%m-%dT%H:%M:%S%z")
+  ts = timestamp($filename)
   u = "#{$url}/pictures/upload?id=#{$id}&token=#{$token}&time_stamp=#{ts}&motion_sensor=#{$motion_sensor.to_s}"
   cmd = "curl -s -S -X POST -F file=@'#{$filename}' '#{u}'"
   system(cmd)
   puts
+end
+
+def timestamp(filename)
+  time = filename.split(/-/)[1]
+  year   = time[0..3]
+  month  = time[4..5]
+  day    = time[6..7]
+  hour   = time[8..9]
+  minute = time[10..11]
+  second = time[12..13]
+  Time.local( year, month, day, hour, minute, second).strftime("%Y-%m-%dT%H:%M:%S%z")
 end
 
 $filename = ARGV[0] # saved file name
