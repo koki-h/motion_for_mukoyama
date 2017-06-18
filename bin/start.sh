99feaccc17bf4d26615854078069c61c2a318c46
koki_h <gohannnotomo@gmail.com>
2017-06-18 11:02:33 +0900

    ログ出力先を設定で変更できるようにした。/dev/nullを設定すればログ出力なし。 resolved #1

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source mukoyama.conf
-./lib/mk_motionconf.rb > motion/work/motion.conf
-nohup motion -c motion/work/motion.conf &
+mkdir -p $MOTION_HOME/motion/work
+$MOTION_HOME/lib/mk_motionconf.rb > $MOTION_HOME/motion/work/motion.conf
+nohup motion -c motion/work/motion.conf > $STD_LOG_FILE 2> $ERR_LOG_FILE &
