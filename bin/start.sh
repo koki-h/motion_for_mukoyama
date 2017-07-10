01a9a933f58c6361802d75591e275b0987c824ca
koki_h <gohannnotomo@gmail.com>
2017-07-10 18:12:12 +0900

    ストリーミングのフォワーディングをautosshを使用するように変更

--- bin/start.sh
+++ bin/start.sh
@@ -47,5 +47,5 @@ $MOTION_HOME/lib/mk_motionconf.rb > $MOTION_HOME/motion/work/motion.conf
 
 spawn "motion -c motion/work/motion.conf" $PID_MOTION
 if [ -n "$REMOTE_STREAMING_HOST" ]; then
-  spawn "ssh -o ServerAliveInterval=60 -N $REMOTE_STREAMING_HOST -R $REMOTE_STREAMING_PORT:localhost:8081" $PID_STREAM
+  spawn "autossh -N -R $REMOTE_STREAMING_PORT:localhost:8081 $REMOTE_STREAMING_HOST" $PID_STREAM
 fi
